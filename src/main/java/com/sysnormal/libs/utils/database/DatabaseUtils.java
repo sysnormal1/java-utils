ec6d8bf6f8d7ac99aec74cdf0a36b448972a1619
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2026-01-01 10:27:23 -0300

    added databaseutils method

--- src/main/java/com/sysnormal/libs/utils/database/DatabaseUtils.java
+++ src/main/java/com/sysnormal/libs/utils/database/DatabaseUtils.java
@@ -1,5 +1,7 @@
 package com.sysnormal.libs.utils.database;
 
+import org.springframework.util.StringUtils;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -9,4 +11,18 @@ public abstract class DatabaseUtils {
     public abstract boolean indexExists(Connection conn, String tableName, String constraintName) throws SQLException;
 
     public abstract boolean foreignKeyExists(Connection conn,String tableName,String fkName) throws SQLException;
+
+    public static String detectQueryType(String query) {
+        String result = "select";
+        if (StringUtils.hasText(query)) {
+            if (query.trim().toLowerCase().indexOf("insert") == 0) {
+                result = "insert";
+            } else if (query.trim().toLowerCase().indexOf("update") == 0) {
+                result = "update";
+            } else if (query.trim().toLowerCase().indexOf("delete") == 0) {
+                result = "delete";
+            }
+        }
+        return result;
+    }
 }
